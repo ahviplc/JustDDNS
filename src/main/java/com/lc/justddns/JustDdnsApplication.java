fb96d8db4dad7674eeb354b47b840888597000c7
ahviplc <ahlc@sina.cn>
2021-10-11 09:40:49 +0800

    first commit.

---
+++ src/main/java/com/lc/justddns/JustDdnsApplication.java
@@ -0,0 +1,13 @@
+package com.lc.justddns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustDdnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustDdnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lc/justddns/JustDdnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lc.justddns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustDdnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
